7814008d6b8cd30b5c74c7095170a94cf6a35a54
sohilp2010 <77651250+sohilp2010@users.noreply.github.com>
2022-03-23 20:54:30 -0400

    Add files via upload

---
+++ book.js
@@ -0,0 +1,52 @@
+status="";
+img="";
+objects=[];
+
+
+ function preload(){
+     img = loadImage('Book.webp');
+
+ }
+
+ function setup()
+ {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectdetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+console.log("Model is loaded");
+status = true;
+objectdetector.detect(img, gotresult);
+}
+
+function gotresult(error, results)
+{
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+    }
+    
+    function draw() 
+    {
+      image(img, 0, 0, 740, 480);
+    
+          if(status != "")
+          {
+            for (i = 0; i < objects.length; i++) 
+            {
+                document.getElementById("status").innerHTML = "Status : Object Detected";
+              fill("#FF0000");
+              percent = floor(objects[i].confidence * 100);
+              text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+              noFill();
+              stroke("#FF0000");
+              rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+          }
+    }
